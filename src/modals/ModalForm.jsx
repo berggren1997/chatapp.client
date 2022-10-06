f03a00cdd930a34072c79480661437f475b68535
philip berggren <philip.berggren@outlook.com>
2022-10-06 20:47:53 +0200

    folders + styling

---
+++ src/modals/ModalForm.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  FormControl,
+  Input,
+  FormLabel,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+const ModalForm = ({ isOpen }) => {
+  const { onClose } = useDisclosure();
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader color="black">New conversation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6} color="blackAlpha.400">
+            <FormControl>
+              <FormLabel>First name</FormLabel>
+              <Input placeholder="First name" />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Last name</FormLabel>
+              <Input placeholder="Last name" />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalForm;
